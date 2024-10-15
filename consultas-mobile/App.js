a86a023b33a61685c342d9a7f185e4353c449010
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-10-14 23:53:29 -0300

    organizando projeto para novas tasks

--- consultas-mobile/App.js
+++ consultas-mobile/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Button from './src/components/button/button';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+  
+  return <>
+      <Text>Exibição Inicial no Expo</Text>
+
+      <Button text="Isso é um teste"/>
+        
+  </>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ consultas-mobile/src/components/button/button.jsx
@@ -0,0 +1,8 @@
+
+import { Text } from "react-native";
+
+function Button(props) {
+    return <Text>{props.text}</Text>
+}
+
+export default Button;
