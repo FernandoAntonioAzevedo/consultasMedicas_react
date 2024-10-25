5f08b43c3c4fe5e09a7a45fa091974d14bd7ac7b
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-10-25 16:00:48 -0300

    Finalizando interações entre screens e iniciando task de acesso aos dados da API

--- consultas-mobile/src/screens/abahome/abahome.jsx
+++ consultas-mobile/src/screens/abahome/abahome.jsx
@@ -2,9 +2,14 @@ import { FlatList, Text, View } from "react-native";
 import { styles } from "./abahome.style.js";
 import { doctors } from "../../constants/data.js";
 import Doctor from "../../components/doctor/doctor.jsx";
-import icon from "../../constants/icon.js";
+
 
 function AbaHome() {
+
+    function ClickDoctor(id_doctor, name, specialty, icon) {
+        console.log(id_doctor, name, specialty, icon);
+    }
+
     return <View style={styles.container}>
         <Text style={styles.text}>Agende os seus serviços médicos</Text>
 
@@ -12,9 +17,11 @@ function AbaHome() {
             keyExtractor={(doc) => doc.id_doctor}
             showsVerticalScrollIndicator={false}
             renderItem={({ item }) => {
-                return <Doctor name={item.name}
-                    icon={item.icon == "M" ? icon.male : icon.female}
-                    specialty={item.specialty} />
+                return <Doctor id_doctor={item.id_doctor}
+                name={item.name}
+                    icon={item.icon} // Masc ou Fem
+                    specialty={item.specialty} 
+                    onPress={ClickDoctor} />
             }} />
     </View>
 }
